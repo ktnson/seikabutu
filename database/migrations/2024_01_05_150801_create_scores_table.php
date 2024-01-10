50633ab6aa148381f33e5a7aaddf084249a9d167
ktnson <ichigo424shion@gmail.com>
2024-01-10 12:46:15 +0900

    table basic

--- database/migrations/2024_01_05_150801_create_scores_table.php
+++ database/migrations/2024_01_05_150801_create_scores_table.php
@@ -15,7 +15,9 @@ return new class extends Migration
     {
         Schema::create('scores', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
